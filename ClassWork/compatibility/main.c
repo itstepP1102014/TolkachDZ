44e08ca71501079bdcf43b81e3594d9447e69154
MetalMisha <possessedsoul@mail.ru>
2015-01-21 11:30:42 +0000

    classwork

---
+++ ClassWork/compatibility/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../modules/compatibility.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    char enter;
+    scanf("%c", &enter);
+    universalClear();
+    printf("Farawell all :'(((\n");
+    universalSeep(2.5);
+    universalClear();
+    return 0;
+}
