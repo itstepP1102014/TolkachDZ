b514f672d1be1af3268209f72b3d1bba3792c463
MetalMisha <possessedsoul@mail.ru>
2015-02-05 21:02:15 +0300

    deliryum

---
+++ HomeWork3/4userEnterNnumber/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter your amt numbers: ", n);
+    scanf("%d", &n);
+
+    for (int i=1, j=0 + ; i<=n; ++i)
+    {
+
+        printf(" \n");
+
+
+
+    }
+    return 0;
+}
