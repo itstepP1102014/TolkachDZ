15a49862c77d08955d1eaa333ca3ba6968df8401
MetalMisha <possessedsoul@mail.ru>
2015-03-04 11:57:50 +0000

    cw

---
+++ ClassWork/main /main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    for(int i = 0;i < argc; ++i)
+        printf("%s\n", argv[i]);
+  //  char *help="The programm created for funny on you=)";
+
+ /*   if(strcmp(argv[1], "--help")==0)
+    {
+        printf("The programm created for funny on you=)\n");
+    }
+*/
+    return 0;
+}
+
+/*bin/Debug/main mama myla ramu*/
